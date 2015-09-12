53c6d82100a452feb36e6b0bb9e3aac1ab545916
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-09-12 22:16:16 +0100

    another travis test fix

--- src/UthandoAdmin/View/Phpinfo.php
+++ src/UthandoAdmin/View/Phpinfo.php
@@ -27,8 +27,8 @@ class PhpInfo extends AbstractHelper
 		ob_end_clean();
 
 		preg_match_all('#<body[^>]*>(.*)</body>#siU', $phpInfo, $output);
-        var_dump($output);
-        $output = (count($output[1]) > 0) ? implode('', $output[1]) : '';
+
+        $output = (isset($output[1][0])) ? $output[1][0] : '';
 		$output = preg_replace('#<table#', '<table class="table table-bordered"', $output);
 		$output = preg_replace('#(\w),(\w)#', '\1, \2', $output);
 		$output = preg_replace('#border="0" cellpadding="3" width="600"#', '', $output);

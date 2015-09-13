70d61a8865d118bd82782682b5331c0c31a2fd61
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-09-13 07:28:32 +0100

    file name was wrong

--- src/UthandoAdmin/View/Phpinfo.php
+++ src/UthandoAdmin/View/Phpinfo.php
@@ -21,25 +21,20 @@ class PhpInfo extends AbstractHelper
 { 
     public function __invoke()
     {
-        $output = '';
-
-        /*ob_start();
+        ob_start();
         phpinfo();
         $phpInfo = ob_get_contents();
-        ob_end_clean();*/
+        ob_end_clean();
 
-        /*preg_match_all('#<body[^>]*>(.*)</body>#siU', $phpInfo, $output);
-               
-        if (is_array($output[1]) && !empty($output[1])) {
-            $output = preg_replace('#<table#', '<table class="table table-bordered"', implode('', $output[1]));
-            $output = preg_replace('#(\w),(\w)#', '\1, \2', $output);
-            $output = preg_replace('#border="0" cellpadding="3" width="600"#', '', $output);
-            $output = preg_replace('#<hr />#', '', $output);
-            $output = str_replace('<div class="center">', '', $output);
-            $output = str_replace('</div>', '', $output);
-        } else {
-            $output = 'PHP info not available';
-        }*/
+        preg_match_all('#<body[^>]*>(.*)</body>#siU', $phpInfo, $output);
+
+        $output = (count($output[1]) > 1) ? implode('', $output[1]) : 'PHP info not available';
+        $output = preg_replace('#<table#', '<table class="table table-bordered"', $output);
+        $output = preg_replace('#(\w),(\w)#', '\1, \2', $output);
+        $output = preg_replace('#border="0" cellpadding="3" width="600"#', '', $output);
+        $output = preg_replace('#<hr />#', '', $output);
+        $output = str_replace('<div class="center">', '', $output);
+        $output = str_replace('</div>', '', $output);
 
         return $output;
     }
